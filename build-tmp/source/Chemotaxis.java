e477970106b6af2c619af2b8086f56fdf39b3354
kiaragil <kiarargil@gmail.com>
2015-09-22 11:38:08 -0700

    done maybe

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony = new Bacteria[100];
-void setup(){     
+public void setup(){     
 	size(600, 600);
 	background(255);
 	for(int i = 0; i < colony.length; i++){
 		colony[i] = new Bacteria();
 	} //end loop 
  } //end setup 
- void draw(){    
+ public void draw(){    
  	fill(252, 225, 155);
  	rect(0, 0, 600,600);
  	for(int i = 0; i < colony.length; i++){
@@ -26,13 +42,13 @@ class Bacteria
  		bacSize = (int)(Math.random()*10)+5;
  	} //end bac constructor
 
- 	void show(){
+ 	public void show(){
  		stroke(0, rgb, 0);
  		fill(0, rgb, 0);
  		ellipse(bacX,bacY, bacSize, bacSize);
  	} //end show
 
- 	void move()
+ 	public void move()
  	{
  			if (bacX < mouseX){
  				bacX += (int)(Math.random()*4)+3;
@@ -52,3 +68,12 @@ class Bacteria
 
  } // end bac class
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
